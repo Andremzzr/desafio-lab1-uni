30b403619eb685acfcc884428ee279febff67244
Arthur Eder <arthureder88@gmail.com>
2025-05-30 19:16:06 -0300

    Update: add Data
    Create Data.java

---
+++ Data.java
@@ -0,0 +1,82 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        if (dataValida(dia, mes, ano)) {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        } else {
+            System.out.println("Data inválida! A data será ajustada para 1/1/2000.");
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        }
+    }
+
+    public boolean verificaAnoBissexto() {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+
+    private boolean dataValida(int dia, int mes, int ano) {
+        if (mes < 1 || mes > 12 || dia < 1) {
+            return false;
+        }
+
+        int[] diasPorMes = {31, verificaAnoBissextoAux(ano) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        return dia <= diasPorMes[mes - 1];
+    }
+
+    private boolean verificaAnoBissextoAux(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+
+    // Getters
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    // Setters
+
+    public void setDia(int dia) {
+        if (dataValida(dia, this.mes, this.ano)) {
+            this.dia = dia;
+        } else {
+            System.out.println("Dia inválido para o mês e ano atual.");
+        }
+    }
+
+    public void setMes(int mes) {
+        if (dataValida(this.dia, mes, this.ano)) {
+            this.mes = mes;
+        } else {
+            System.out.println("Mês inválido para o dia e ano atual.");
+        }
+    }
+
+    public void setAno(int ano) {
+        if (dataValida(this.dia, this.mes, ano)) {
+            this.ano = ano;
+        } else {
+            System.out.println("Ano inválido para o dia e mês atual.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;
+    }
+
+}
