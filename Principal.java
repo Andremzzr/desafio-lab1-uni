c80a29592b59004ebb9234dcaa47cc1af8b8ef09
Arthur Eder <arthureder88@gmail.com>
2025-05-31 22:51:15 -0300

    Update: add classe principal

---
+++ Principal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        Loja lojaCriada = null;
+        Produto produtoCriado = null;
+        
+    }
+    
+}
