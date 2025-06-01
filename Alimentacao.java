f0fb6c9e32beef7d364095991213df14a20261db
Arthur Eder <arthureder88@gmail.com>
2025-06-01 01:40:56 -0300

    update: att

--- Alimentacao.java
+++ Alimentacao.java
@@ -2,9 +2,9 @@ public class Alimentacao extends Loja {
 
     private Data dataAlvara;
 
-    public Alimentacao (String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, Data dataAlvara)
+    public Alimentacao (String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, Data dataAlvara, int qtdMaxProdutos)
  {  
-    super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+    super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, qtdMaxProdutos);
     this.dataAlvara = dataAlvara;
 
  }
